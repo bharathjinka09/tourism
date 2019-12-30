a67db64d089ed4876ed7da4b55648eeb85f7bf4a
bharathmieone <bharath@mieone.com>
2019-12-30 11:34:21 +0530

    Changed admin panel title

--- main/urls.py
+++ main/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bharath.urls'))
 ]
+
+admin.site.site_header = 'Tourism Admin Panel'                    # default: "Django Administration"
+admin.site.index_title = 'Tourism'                 # default: "Site administration"
+admin.site.site_title = 'Tourism Admin Panel' # default: "Django site admin"
